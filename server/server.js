b4958c9d107c79d7b18e887d578f2c90df57f159
Andrii Polunin <apolunin@gmail.com>
2018-01-11 15:16:18 +0200

    Prepare application for Heroku deployment

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
@@ -9,5 +10,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('server is up');
+  console.log(`server is up on port ${port}`);
 });
